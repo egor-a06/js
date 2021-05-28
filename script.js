af6d4cfcfda63c8db33a5379081bec6dad3fd802
Egor Andreev <egorandreev2310@gmail.com>
2021-05-28 17:25:01 +0500

    сделал ДЗ по 2 уроку

---
+++ script.js
@@ -0,0 +1,24 @@
+let money = 6000;
+let income = "такси";
+let addExpenses = "день рождения бабушки, поход в кино, порвал штаны, купил хомяка";
+let deposit = false;
+let mission = 99999999999;
+let period = 9;
+
+console.log(money);
+console.log(income);
+console.log(addExpenses);
+console.log(deposit);
+console.log(mission);
+console.log(period);
+
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split());
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+
+console.log("Период равен " + period + " месяцев");
+
+console.log("Цель заработать " + mission + " рублей");
